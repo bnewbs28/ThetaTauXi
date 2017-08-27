cfffeeefb9097da84206bab43686dfb27ea1a3e8
bnewbs28 <bnewbs28@gmail.com>
2017-08-27 13:45:15 -0500

    second commit

---
+++ web/wp-content/themes/thetatautheme/home-page.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Template Name: Home
+ * The template for displaying the home page.
+ *
+ * @package WordPress
+ * @subpackage us_lighting_theme
+ */
+
+get_header();
+
+the_post();
+?>
+
+<h1>Welcome Home!</h1>
+
+<?php get_footer(); ?>
